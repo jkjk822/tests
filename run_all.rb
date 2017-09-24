0db4ae1a12da560f41e8c77f9acef493e22c008b
River <jkjk822@yahoo.com.au>
2017-09-24 18:19:50 -0400

    Support for spec style minitests

--- run_all.rb
+++ run_all.rb
@@ -1,4 +1,4 @@
 require_relative 'triple'
 
-Dir['tests/*[^spec].rb'].each {|file| require_relative file }
+Dir['tests/*[^s][^p][^e][^c].rb'].each {|file| require_relative file }
 Dir['tests/*spec.rb'].each {|file| require_relative file }
